1199405d7cc5c394d9763b1419ed1f58dc3c22db
rserafael <rafaelsantos.rse@gmail.com>
2019-01-10 20:57:08 -0200

    vue scanning is working

--- utilities.py
+++ utilities.py
@@ -3,7 +3,6 @@ import time
 from selenium.webdriver import Chrome, ChromeOptions
 import pyscreenshot
 
-
 base_directory = os.path.dirname(os.path.abspath(__file__))
 chrome_path = os.path.join(base_directory, 'chromedriver_linux64/chromedriver')
 scrolldown_js = '''
@@ -34,54 +33,20 @@ def start_chrome_driver(chrome_path = chrome_path, headless = False):
             print("An error has occurred")
             raise err
 
-def scroll_down_to_the_bottom(driver, printscreen=False, file_name='file'):
-    count = 1
-    while True:
-        try:
-            driver.execute_script(scrolldown_js)
-            time.sleep(0.2)
-            if printscreen:
-                img = pyscreenshot.grab()
-                img.save('{1}{0}.png'.format(count, file_name))
-                count+=1
-        except Exception as err:
-            alert = driver.switch_to.alert
-            alert.accept()
-            break
-
-    # driver.execute_script('''console.log('done')''')
-    # actions = ActionChains(driver)
-    # actions.key_down(Keys.ENTER)
-    # actions.perform()
-#
-# if __name__ == "__main__":
-#     driver = start_chrome_driver(chrome_path)
-#     scan_vue_documentation(driver)
-#     count = 1
-#     while True:
-#         try:
-#             scrolldown(driver, count)
-#             count+=1
-#         except Exception as err:
-#             print(err)
-#             print("------------")
-#             for prop in dir(err):
-#                 print(prop)
-#             print("-------------")
-#             break
-#     print("We breaked")
-#     time.sleep(2)
-#     alert = driver.switch_to.alert
-#     alert.accept()
-#     time.sleep(1)
-#     print("done :)")
-#     driver.get("http://google.com")
-#     actions = ActionChains(driver)
-#     actions.key_down(Keys.ENTER)
-#     actions.key_up(Keys.ENTER)
-#     actions.perform()
-#     print("And it is done.")
-#     time.sleep(3)
-#     driver.quit()
-#     print("The end!...")
-
+def scroll_down_to_the_bottom(driver, printscreen=False, file_path=None):
+    if type(file_path) != str:
+        raise TypeError("file_path must be an string")
+    else:
+        count = 1
+        while True:
+            try:
+                time.sleep(0.2)
+                if printscreen:
+                    img = pyscreenshot.grab()
+                    img.save('{1}{0}.png'.format(count, file_path))
+                    count+=1
+                driver.execute_script(scrolldown_js)
+            except Exception as err:
+                alert = driver.switch_to.alert
+                alert.accept()
+                break
--- vuescanner.py
+++ vuescanner.py
@@ -3,6 +3,10 @@ import time
 from selenium.webdriver import Chrome
 from utilities import scroll_down_to_the_bottom, start_chrome_driver, chrome_path
 
+# Some Global Variables
+main_vue_folder = "./vue_get_started/"
+page_folder_name = "vuepage"
+
 def scan_vue_documentation(driver = None):
     global define_function_js, scrolldown_js, show_defined_function
     if type(driver) != Chrome:
@@ -10,12 +14,50 @@ def scan_vue_documentation(driver = None):
     else:
         driver.get('https://vuejs.org/v2/guide/')
 
+def find_next_link(driver = None):
+    if type(driver) != Chrome:
+        raise TypeError("driver must be a Chrome instance")
+    else:
+        try:
+            guide_links = driver.find_elements_by_class_name("guide-links")
+            print("length of guide_links = {0}".format(len(guide_links)))
+            guide_link = guide_links[0]
+            print("guide_link = {0}".format(guide_link))
+            children = guide_link.get_property("children")
+            print("length of children = {0}".format(len(children)))
+            next_link = children[1]
+            print("next_link = {0}".format(next_link))
+            return next_link
+        except Exception as err:
+            print(err)
+            return None
+
+def scan_single_page(number):
+    if type(number) == int:
+        folder_path = os.path.join(main_vue_folder, page_folder_name + "{0}/".format(number))
+        file_path = os.path.join(folder_path, "vue")
+        print("folder_path = {0} and file_path = {1}".format(folder_path, file_path))
+        os.mkdir(folder_path)
+        scroll_down_to_the_bottom(driver, True, file_path)
+    else:
+        raise TypeError("number param must be an integer.")
+
 if __name__ == '__main__':
     driver = start_chrome_driver(chrome_path, False)
     scan_vue_documentation(driver)
     driver.fullscreen_window()
-    # os.mkdir("./vuefiles1/")
-    # scroll_down_to_the_bottom(driver, True, './vuefiles1/vue')
+    os.mkdir(main_vue_folder)
+
+    count = 1
+    scan_single_page(count)
+    next_link = find_next_link(driver)
+
+    while next_link != None:
+        count += 1
+        next_link.click()
+        time.sleep(0.5)
+        scan_single_page(count)
+        next_link = find_next_link(driver)
     driver.quit()
 
 
